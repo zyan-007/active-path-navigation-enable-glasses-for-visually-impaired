bf8f5b1778a840edd311b7edb784bd624a9e29e2
zyan-007 <zyanprogramming@gmail.com>
2026-04-17 18:22:18 +0530

    test new changes

--- remote/glasses/test.py
+++ remote/glasses/test.py
@@ -1,7 +1,13 @@
 import serial
+import os
+import time
+
+# pre-configure port before opening
+os.system('sudo stty -F /dev/ttyS0 9600 cs8 -cstopb -parenb')
+time.sleep(0.5)
 
 try:
-    ser = serial.Serial('/dev/ttyS0', 115200, timeout=1)
+    ser = serial.Serial('/dev/ttyS0', 9600, timeout=1)
     print("Serial port opened")
 
     while True:
